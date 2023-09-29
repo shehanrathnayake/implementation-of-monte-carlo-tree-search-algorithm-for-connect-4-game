c7493cbf6b5d81a79a51b1985bd2197fd6f18963
Shehan Rathnayake <shehandj.swd@gmail.com>
2023-09-29 16:08:07 +0530

    Add comments

--- src/main/java/lk/ijse/dep/service/AiPlayer.java
+++ src/main/java/lk/ijse/dep/service/AiPlayer.java
@@ -14,6 +14,8 @@ public class AiPlayer extends Player{
 
     @Override
     public void movePiece(int col) {
+
+        /* ====================Class Node representing nodes in the tree============================================== */
         class Node {
             private int col;
             private Node parent = null;
@@ -22,18 +24,25 @@ public class AiPlayer extends Player{
             private double ucbValue = 0;
             private ArrayList<Node> childArray = new ArrayList<>();
 
-            private Node() {
-//                this.col = col;
-            }
+            private Node() {}
             private Node(int col, Node parent) {
                 this.col = col;
                 this.parent = parent;
             }
         }
 
+        /* ================Class that implemented MonteCarlo Tree Search Algorithm==================================== */
         class MonteCarloTreeSearch {
             Node rootNode;
             Node maxUcbNode = null;
+
+            /*              Selection phase
+
+            * If root comes for the first iteration, he has no child, so send him for expansion process. Here where tree is started to be built.
+            * From the second iteration root has children array so checks the children one by one whether they have been visited at least one. if not send the first non visited child for rollout process
+            * If all children have been visited, then select the child that have higher UCB value. if he has children, send him for select method as a parent node (Recursion). Else send him for expansion process
+            *
+            * */
             private void select(Node parentNode) {
                 ArrayList<Node> childList = parentNode.childArray;
                 if (childList.size() > 0) {
@@ -55,16 +64,28 @@ public class AiPlayer extends Player{
                 } else expand(parentNode);
             }
 
+            /*              Expansion phase
+
+            * Add 5 child nodes to the child arraylist. (Only if it is a legal move)
+            * Then send the first child for rollout process
+            *
+            * */
             private void expand(Node parentNode) {
                 for (int i = 0; i < 6; i++) {
-                    System.out.println(i + "th branch");
                     if (board.isLegalMove(i)){
                         parentNode.childArray.add(new Node(i, parentNode));
                     }
                 }
-//                System.out.println("Child array size: " + parentNode.childArray.size());
                 rollOut(parentNode.childArray.get(0));
             }
+
+            /*              Rollout phase
+
+             * Creating a logical board with the current status and play a simulation with random move until a terminal state coming (win, lose or tie)
+             * Calculating the UCT value (win/visits)
+             * Send the rollout node and UCT value for back propagation
+             *
+             * */
 
             private void rollOut(Node nonVisitedNode) {
 
@@ -74,14 +95,13 @@ public class AiPlayer extends Player{
                 // Simulate a random game starting from the current state
                 int visits = 0;
                 while (logicalBoard.findWinner().getWinningPiece() == Piece.EMPTY  && logicalBoard.existLegalMoves()) {
-                    int move = 0;
                     int randomMove;
                     do{
                         randomMove = (int) (Math.random() * 10);
                     } while(!(randomMove >= 0 && randomMove < 6 && logicalBoard.isLegalMove(randomMove)));
 
                     logicalBoard.updateMove(randomMove, currentPlayer);  // Update with the current player's piece
-//                    logicalBoard.getBoardUI().update(randomMove, (currentPlayer == Piece.GREEN) ? false : true);
+
                     currentPlayer = (currentPlayer == Piece.GREEN) ? Piece.BLUE : Piece.GREEN;  // Switch to the other player
                     visits++;
                 }
@@ -91,27 +111,34 @@ public class AiPlayer extends Player{
                 backPropagate(nonVisitedNode, uctValue);
             }
 
+            /*              Back propagation phase
+
+             * Traversing to the root updating new UCT value, no of visits and new UCB values
+             * Calling to update maxUsbNode which hold the highest UCB value among all the visited nodes in the entire tree.
+             *
+             * */
+
             private void backPropagate(Node rolledOutNode, double uctValue) {
                 Node traversingNode = rolledOutNode;
                 traversingNode.uctValue += uctValue;
                 traversingNode.visits ++;
                 Node parentTraversingNode;
+
                 while(traversingNode.parent != null) {
                     parentTraversingNode = traversingNode.parent;
                     parentTraversingNode.uctValue += uctValue;
                     parentTraversingNode.visits ++;
                     traversingNode.ucbValue = traversingNode.uctValue + 2 * sqrt(Math.log(traversingNode.parent.visits) / traversingNode.visits);
-
-//                    System.out.println("updated uct value: " + traversingNode.uctValue);
-//                    System.out.println("updated math value: " + 2 * sqrt(Math.log(traversingNode.parent.visits) / traversingNode.visits));
-//                    System.out.println("updated ucb value" + traversingNode.ucbValue);
-//                    System.out.println("updated parent visits: " + traversingNode.parent.visits);
-//                    System.out.println();
-
                     traversingNode = traversingNode.parent;
                 }
                 updateMaxUcbNode(rootNode);
             }
+
+            /*              Updating maxUCBNode
+
+             * Find the node which has the highest value of UCB value among nodes that have been visited at least one by traversing the entire tree.
+             *
+             * */
 
             private void updateMaxUcbNode(Node node) {
                 if (node == rootNode && node.childArray.size() == 0) return;
@@ -127,26 +154,45 @@ public class AiPlayer extends Player{
                 }
             }
 
+            /*              Avoid defeat
+
+             * Check whether human player has connected 3 consecutive pieces which he is going to win in the next move of him
+             * If so, set the next move to avoid it. Return the required column. Else return -1
+             *
+             * */
+
             private int avoidDefeatMove() {
-//                System.out.println();
                 return -1;
             }
+
+            /*              Best move
+
+             * Starting point
+             * Check whether avoidDefeatMove() returns column number or -1
+             * If it returns column number, returns it. Else continue Monte Carlo Tree search algo and returns the best move column number
+             *
+             * */
 
             private int bestMove() {
                 int bestMove = avoidDefeatMove();
                 if (bestMove == -1) {
                     rootNode = new Node();
-                    for (int i = 0; i < 1000; i++) {
+                    for (int i = 0; i < 10000; i++) {
                         select(rootNode);
                     }
                     bestMove = maxUcbNode.col;
                 }
-
-//                System.out.println("maxucb node: " + maxUcbNode);
-//                System.out.println("BestMove ucb value: " + maxUcbNode.ucbValue);
                 return bestMove;
             }
         }
+
+        /*===================================================================================================================================================*/
+
+        /*              AiPlayer Class
+         * Check whether there is only one column available for the next move
+         * If so, set it to the next move. Else making an instance of MonteCarloTreeSeach class and get returned the best move
+         *
+         * */
 
         int count = 0;
         int availableCol = 0;
@@ -157,16 +203,9 @@ public class AiPlayer extends Player{
         }
         if (count == 5) {
             col = availableCol;
-//            System.out.println("count: " + col);
         } else col = new MonteCarloTreeSearch().bestMove();
 
-
-//        int number;
-//        do{
-//            number = (int) (Math.random() * 10);
-//        } while(!(number >= 0 && number < 6 && board.isLegalMove(number)));
-//
-//        col = number;
+        /* Rest of the AiPlayer class*/
 
         board.updateMove(col, Piece.GREEN);
         board.getBoardUI().update(col, false);
